e0b65923dc255b6c7185ad1863cb6bfb6d808023
renav2 <kulkarnirenav255@gmail.com>
2021-07-04 23:15:46 +0530

    This app is made to complete Task #5 of Graduate Rotational Internship Program (GRIP) of The Sparks Foundation i.e.,Social Media Integration in an android app.

---
+++ app/src/main/java/com/example/internship/startpage.java
@@ -0,0 +1,35 @@
+package com.example.internship;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class startpage extends Activity {
+
+    TextView designed, name, app_name;
+    ImageView logo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startpage);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+
+            }
+        }, 3800);
+    }
+
+//hbdjvhd
+}
